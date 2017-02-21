76b9190bacff56d579d49cb8e6ca0ed6dcb6cf3d
Theta Wang <thetawang@thetademac-mini.local>
2017-02-21 11:39:39 +0800

    Create app for test swift

--- HiSwift/ViewController.swift
+++ HiSwift/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var MainLabel: UILabel!
+    @IBOutlet weak var MainButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func showMessage() {
+        MainLabel.text!="hi"
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
