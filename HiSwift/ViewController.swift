177880c7eb6c4403318ec57f978c88548ba29f05
Theta Wang <thetawang@thetademac-mini.local>
2017-03-01 02:21:49 +0800

    update

--- HiSwift/ViewController.swift
+++ HiSwift/ViewController.swift
@@ -27,37 +27,15 @@ class ViewController: UIViewController {
     }
     @IBAction func HTTPRequest(_ sender: Any) {
         // Set up the URL request
-        let todoEndpoint: String = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.4.js"
-        guard let url = NSURL(string: todoEndpoint) else {
-            print("Error: cannot create URL")
-            return
+        let url = NSURL(string: "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.4.js")
+        
+        let task = URLSession.shared.dataTask(with: url! as URL) {(data, response, error) in
+            print(NSString(data: data!, encoding: String.Encoding.utf8.rawValue)!)
         }
-        let urlRequest = NSURLRequest(url: url as URL)
         
-        // set up the session
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-        
-        // make the request
-        let task = session.dataTask(with: urlRequest as URLRequest, completionHandler: { (data, response, error) in
-            
-            if let error = error {
-                print(error)
-            }
-            
-            if let data = data{
-                print("data =\(NSString(data: data, encoding:String.Encoding.utf8.rawValue) as String?)")
-            }
-            if let response = response {
-                
-                //print("response = \(response)")
-                let httpResponse = response as! HTTPURLResponse
-                print("response code = \(httpResponse.statusCode)")
-            }
-            
-            
-        })
         task.resume()
+        
+        
         
     }
     
